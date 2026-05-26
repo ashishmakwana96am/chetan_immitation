b9f578194268b4353fa00a844c69c190e8f9755d
Ashish Makwana <amakwana96.am@gmail.com>
2026-05-26 14:34:56 +0530

    sidebar name proeprly show

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -9,7 +9,9 @@
           <path fill-rule="evenodd" clip-rule="evenodd" d="M7.77295 16.3566L23.6563 0H32V6.88383C32 6.88383 31.8262 9.17836 30.6591 10.4057L19.7824 22H13.6938L7.77295 16.3566Z" fill="#7367F0" />
         </svg>
       </span>
-      <span class="app-brand-text ecom menu-text fw-bold">Chetan Immitation</span>
+      <span class="app-brand-text ecom menu-text fw-bold" style="font-size: 22px; line-height: 1.2; white-space: normal; text-align: center; padding-left: 5px;">
+        Chetan<br>Immitation
+      </span>
     </a>
     <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
       <i class="ti menu-toggle-icon d-none d-xl-block ti-sm align-middle"></i>
