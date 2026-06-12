7d9fb5a8835d96157bda53e9cf13858881b600fe
Ashish Makwana <amakwana96.am@gmail.com>
2026-06-12 17:02:35 +0530

    status switch issue fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -95,6 +95,9 @@
 
 
     <script src="{{ asset('assets/vendor/libs/jquery/jquery.js') }}"></script>
+    <script>
+        $.ajaxSetup({ cache: false });
+    </script>
     <script src="{{ asset('assets/vendor/libs/popper/popper.js') }}"></script>
     <script src="{{ asset('assets/vendor/js/bootstrap.js') }}"></script>
     <script src="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
