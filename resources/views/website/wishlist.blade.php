72f7b25221902fef7f5f929675e9eda7b1772afc
payalptoliya416 <payal.rising416@gmail.com>
2026-06-20 12:05:11 +0530

    text changes

--- resources/views/website/wishlist.blade.php
+++ resources/views/website/wishlist.blade.php
@@ -115,7 +115,7 @@
 
                             <div class="border-t border-[#D5D5D5] mt-2 pt-2">
                                 <div class="flex items-center flex-wrap gap-2 text-[#3D403F]">
-                                    <button class="add-to-cart-from-wishlist after:border-r after:border-[#3D403F] after:h-5 after:mx-4 after:content-[''] after:inline-block hover:text-[#B4771E] flex items-center gap-[10px] text-base transition"
+                                    <button class="add-to-cart-from-wishlist after:border-r after:border-[#3D403F] after:h-5 after:mx-4 after:content-[''] after:inline-block hover:text-[#B4771E] flex items-center gap-[10px] text-sm sm:text-base transition"
                                         data-product-id="{{ $prod->id }}"
                                         data-variant-id="{{ $variant?->id ?? '' }}">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 sm:size-5 -mt-1">
@@ -127,7 +127,7 @@
                                         data-product-id="{{ $prod->id }}"
                                         data-variant-id="{{ $variant?->id ?? '' }}"
                                         data-toggle-url="{{ route('wishlist.toggle') }}">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 sm:size-4 sm:size-5 -mt-1 -mt-1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 sm:size-5 -mt-1">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                         </svg>
                                         Remove From Wishlist
