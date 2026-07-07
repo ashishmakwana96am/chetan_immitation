e1bb3ff5232dba5f2cfe5cbfd74bba0152b541ed
Jenish4106 <jenish.rising@gmail.com>
2026-07-07 20:46:21 +0530

    sale tooltip changes

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -131,9 +131,21 @@ class SaleController extends Controller
                             . $availabilitySection
                             . "</div>";
                     } else {
+                        $otherRows = $invRows->where('location_id', '!=', $order->location_id)
+                            ->filter(fn ($inv) => $inv->quantity > 0)
+                            ->map(function ($inv) {
+                                return "<div class='sw-branch'><span>" . e($inv->location->name ?? 'Unknown') . "</span><span class='sw-qty'>" . (int) $inv->quantity . "</span></div>";
+                            })
+                            ->implode('');
+
+                        $availabilitySection = $otherRows !== ''
+                            ? "<div class='sw-subtitle'>Available in other branches</div>" . $otherRows
+                            : "<div class='sw-empty'>Not available in any other branch</div>";
+
                         $issueBlocks[] = "<div class='sw-item'>"
                             . "<div class='sw-title'><i class='ti ti-box'></i>" . e($item->product->name ?? 'Product') . "</div>"
-                            . "<div class='sw-status'><i class='ti ti-check'></i>Available at this location</div>"
+                            . "<div class='sw-status'><i class='ti ti-alert-circle'></i>Not available at this location</div>"
+                            . $availabilitySection
                             . "</div>";
                     }
                 }
