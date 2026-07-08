8f55ddb81cdda26f03248852fec774f1557db2f5
payalptoliya416 <payal.rising416@gmail.com>
2026-07-08 15:11:12 +0530

    responsive issue

--- resources/views/website/checkout.blade.php
+++ resources/views/website/checkout.blade.php
@@ -646,7 +646,7 @@
                                 {{ session()->has('applied_coupon_code') ? 'disabled' : '' }}
                                 class="h-[44px] lg:h-[52px] border border-[#D5D5D5] px-2 sm:px-4 bg-white text-base md:text-lg leading-[18px] placeholder:text-base placeholder:md:text-lg rounded-sm flex-grow {{ session()->has('applied_coupon_code') ? 'bg-gray-100 cursor-not-allowed' : '' }} w-full">
                             <button id="couponActionBtn" onclick="handleCouponAction()"
-                                class="bg-[#B4771E] text-white px-4 xl:px-5 h-[44px] lg:h-[52px] whitespace-nowrap text-base transition rounded-sm w-full xl:w-auto">
+                                class="bg-[#B4771E] text-white px-4 h-[44px] lg:h-[52px] whitespace-nowrap text-base transition rounded-sm w-full xl:w-auto">
                                 {{ session()->has('applied_coupon_code') ? 'Remove Coupon' : 'Apply Coupon' }}
                             </button>
                         </div>
