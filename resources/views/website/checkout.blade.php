4e45d5a2f647c0223d10eac19b2a03750b4c452c
payalptoliya416 <payal.rising416@gmail.com>
2026-07-08 14:54:00 +0530

    responisve issue

--- resources/views/website/checkout.blade.php
+++ resources/views/website/checkout.blade.php
@@ -15,7 +15,7 @@
                     Complete your order securely and receive your jewellery at your doorstep.
                 </p>
             </div>
-            <div class="grid grid-cols-1 xl:grid-cols-[minmax(0,1fr)_360px] 2xl:grid-cols-[minmax(0,1fr)_400px] gap-6 items-start">
+            <div class="grid grid-cols-1 lg:grid-cols-[minmax(0,60%)_1fr] 2xl:grid-cols-[953px_1fr] lg:gap-6 items-start">
                 <!-- LEFT SIDE -->
 
                 <div class="min-w-0 space-y-6">
@@ -634,7 +634,7 @@
                 </div>
                 <!-- RIGHT SIDEBAR -->
 
-                <div class="space-y-4 min-w-0 w-full">
+                <div class="space-y-4">
                     <!-- Coupon -->
                     <div class="mb-6">
                         <h3 class="text-lg md:text-xl font-medium mb-3">
