e408b1973237329098cbd50a73138fd976e35cef
Ashish Makwana <amakwana96.am@gmail.com>
2026-08-27 15:47:16 +0530

    purchase bill value changes

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -357,7 +357,8 @@ class DashboardController extends Controller
                     ->whereHas('order', function ($q) use ($locationId) {
                         $q->where('location_id', $locationId)
                           ->where('order_type', 'sale')
-                          ->whereIn('status', [Order::STATUS_APPROVE, Order::STATUS_SHIPPED, Order::STATUS_OUT_FOR_DELIVERY, Order::STATUS_DELIVERED]);
+                          ->whereIn('status', [Order::STATUS_APPROVE, Order::STATUS_SHIPPED, Order::STATUS_OUT_FOR_DELIVERY, Order::STATUS_DELIVERED])
+                          ->whereIn('payment_status', [Order::PAYMENT_STATUS_PAID, Order::PAYMENT_STATUS_PARTIAL]);
                     })
                     ->get();
 
@@ -368,10 +369,14 @@ class DashboardController extends Controller
                     $p = $sItem->product;
                     if (!$p) continue;
 
+                    $purchasePrice = (float) (($sItem->variant->purchase_price ?? 0) > 0 
+                        ? $sItem->variant->purchase_price 
+                        : ($p->purchase_price ?? 0));
+
                     $multiplier = $this->stockMultiplierFor($p, $sItem->pair_type, $sItem->custom_size_value);
                     $quantity   = (int) $sItem->quantity;
 
-                    $soldPurchaseValue += $this->purchasePriceForOrderItem($sItem) * $quantity;
+                    $soldPurchaseValue += $purchasePrice * $quantity;
                     $soldMrpValue      += $this->mrpForOrderItem($sItem, $multiplier) * $quantity;
                 }
 
